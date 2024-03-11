3ac8e13cd11988bcb9276d6325ddc10629fe4138
W-Panther007 <wasim040509@gmail.com>
2024-03-11 16:46:14 +0000

    Card component added with dynamic variables

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,27 @@
 import Banner from './Banner.jsx';
+import Card from './Card.jsx';
 import '../styles/home.css';
+import sunnyImg from '../assets/sunny.png';
+
 
 function Home() {
     
+    // Mapping image 
+    const weatherIcons = {
+        sunny: sunnyImg,
+    };
+
+
+    const city = "London";
+    const temp = 18;
+    const weatherCondition = "Sunny";
+    const feelTemp = 16;
+
     return (
-        <Banner />
+        <>
+            <Banner />
+            <Card city={city.toUpperCase()} weatherIconSrc={weatherIcons["sunny"]} temp={temp} condition={weatherCondition.toUpperCase()} feelTemp={feelTemp} /> 
+        </>
     );
 
 }
