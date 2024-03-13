a57053efe43b0cae644a26cc3e7eb37daa0d2f4e
ec22648 <ec22648@qmul.ac.uk>
2024-03-13 00:52:57 +0000

    widget test on info page

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,8 +1,16 @@
 import '../styles/info.css';
+import sunriseImg from '../assets/sunrise.png'
+import sunsetImg from '../assets/sunset.png'
+import SunriseSunset from './SunriseSunset';
 
 function Info() {
 
-    return(<h1>INFO PAGE</h1>);
+    return(
+        <>
+            <h1>INFO PAGE</h1>
+            <SunriseSunset sunriseIcon={sunriseImg} sunsetIcon={sunsetImg} sunriseTime="06:25" sunsetTime="19:40"/>
+        </>
+    );
 
 }
 
