9f247f30554f2599bfbf7597b3dbae55bce3ea6f
ec22648 <ec22648@qmul.ac.uk>
2024-03-13 00:41:55 +0000

    created the sunrise/sunset widget

---
+++ src/components/SunriseSunset.jsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+import '../styles/sunrisesunset.css'
+
+function SunriseSunset({sunriseIcon, sunsetIcon, sunriseTime, sunsetTime}) {
+    return(
+        <>
+           <div className="sun-container">
+                <div className="sunrise">
+                    <img src={sunriseIcon} alt="Sunrise"/>
+                    <div className="sunrise-time">
+                        <p className="header">SUNRISE</p>
+                        <p>{sunriseTime}</p>
+                    </div>
+                </div>
+                <div className="sunset">
+                    <div className="sunset-time">
+                        <p className="header">SUNSET</p>
+                        <p>{sunsetTime}</p>
+                    </div>
+                    <img src={sunsetIcon} alt="Sunset"/>
+                </div>
+            </div> 
+        </>
+    );
+}
+
+SunriseSunset.PropTypes = {
+    sunriseIcon: PropTypes.string,
+    sunsetIcon: PropTypes.string,
+    sunriseTime: PropTypes.string,
+    sunsetTime: PropTypes.string,
+}
+
+export default SunriseSunset;
