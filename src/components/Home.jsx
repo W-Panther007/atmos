ca2fc1732e2df7320f972903af276c796f04337e
W-Panther007 <wasim040509@gmail.com>
2024-03-14 22:27:18 +0000

    Card Carousel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,10 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
 import Banner from './Banner.jsx';
 import Card from './Card.jsx';
 import '../styles/home.css';
 import sunnyImg from '../assets/sunny.png';
+import 'swiper/css/bundle';
 
 
 function Home() {
@@ -12,15 +15,25 @@ function Home() {
     };
 
 
-    const city = "London";
-    const temp = 20;
-    const weatherCondition = "Sunny";
-    const feelTemp = 18;
+    const cardsData = [
+        { city: "London", weatherIconSrc: weatherIcons["sunny"], temp:20, condition: "Sunny", feelTemp:18 },
+        { city: "London", weatherIconSrc: weatherIcons["sunny"], temp:20, condition: "Sunny", feelTemp:18 },
+        { city: "London", weatherIconSrc: weatherIcons["sunny"], temp:20, condition: "Sunny", feelTemp:18 },
+        { city: "London", weatherIconSrc: weatherIcons["sunny"], temp:20, condition: "Sunny", feelTemp:18 },
+    ];
+
+     
 
     return (
         <>
             <Banner />
-            <Card city={city.toUpperCase()} weatherIconSrc={weatherIcons["sunny"]} temp={temp} condition={weatherCondition.toUpperCase()} feelTemp={feelTemp} /> 
+            <Swiper className="mySwiper" slidesPerView='auto' spaceBetween={30} centeredSlides={true} pagination={{clickable: false, dynamicBullets: true,}} modules={[Pagination]} >
+                {cardsData.map((card, index) => (
+                    <SwiperSlide key={index}>
+                        <Card city={card.city.toUpperCase()} weatherIconSrc={card.weatherIconSrc} temp={card.temp} condition={card.condition.toUpperCase()} feelTemp={card.feelTemp} />
+                    </SwiperSlide>
+                ))} 
+            </Swiper>
         </>
     );
 
