3f8ee7398353418de39bf591e8fcf035be40976f
Sami Ahmed <ec22522@qmul.ac.uk>
2024-03-16 12:08:13 +0000

    hourly weather widget

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,9 +1,19 @@
 import '../styles/info.css';
+import Hourly from './Hourly.jsx';
+
 
 function Info() {
 
-    return(<h1>INFO PAGE</h1>);
 
+    return(
+    <>
+    <h1>INFO PAGE</h1>
+    <br></br>
+    <Hourly/>
+    </>
+    );
+   
 }
+
 
 export default Info;
