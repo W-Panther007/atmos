30fed2734ee9f710ad1ac5ca3073fbc581fb1319
W-Panther007 <wasim040509@gmail.com>
2024-03-17 16:00:26 +0000

    integrated fahims work

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from './App.jsx';
 import { IoIosArrowUp } from "react-icons/io";
+import SunriseSunset from './SunriseSunset.jsx';
+import sunriseImg from '../assets/sunrise.png';
+import sunsetImg from '../assets/sunset.png';
 import '../styles/info.css';
 
 function Info() {
@@ -10,6 +13,7 @@ function Info() {
     return(
         <>
             <IoIosArrowUp className="arrow-up" style={{color: theme === 'light' ? 'white' : 'black'}}/>
+            <SunriseSunset sunriseIcon={sunriseImg} sunsetIcon={sunsetImg} sunriseTime="06:25" sunsetTime="19:40"/>
         </>
     );
 
